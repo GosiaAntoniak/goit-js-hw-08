ab12f7bf955c40bfdfe04acb95be2fdd9a4fa52f
Gosia Antoniak/gosia-antoniak_89@o2.pl <gosia-antoniak_89@o2.pl>
2022-12-01 15:02:37 +0100

    feat: add gallery, video and feedback

---
+++ src/js/02-video.js
@@ -0,0 +1,42 @@
+import Player from '@vimeo/player';
+import _throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onPlay = function (currentTime) {
+  if (currentTime.seconds !== currentTime.duration) {
+    storage.default.save('videoplayer-current-time', currentTime.seconds)
+  } else {
+    console.log("The video is over")
+    storage.default.save('videoplayer-current-time', 0)
+  }
+
+
+};
+player.on('timeupdate', _throttle(onPlay, 1000));
+
+
+player
+  .setCurrentTime(storage.default.load('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
---
+++ src/js/03-feedback.js
@@ -0,0 +1,51 @@
+import _throttle from 'lodash.throttle';
+import * as storage from './storage';
+
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', _throttle(saveMessage, 500));
+
+function saveMessage() {
+  const {
+    elements: { email, message },
+  } = form;
+  const feedback = {
+    email: email.value,
+    message: message.value,
+  };
+  console.log(feedback);
+  storage.default.save(LOCALSTORAGE_KEY, feedback);
+}
+
+const parsedInput = storage.default.load(LOCALSTORAGE_KEY);
+
+window.addEventListener('load', checkStorage);
+
+function checkStorage() {
+  const {
+    elements: { email, message },
+  } = form;
+  if (parsedInput) {
+    email.value = parsedInput.email;
+    message.value = parsedInput.message;
+  } else {
+    email.value = ' ';
+    message.value = ' ';
+  }
+}
+
+form.addEventListener('submit', afterSubmit);
+
+function afterSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  console.log(`Email: ${email.value}, Message: ${message.value}`);
+  form.reset();
+  storage.default.remove(LOCALSTORAGE_KEY);
+}
